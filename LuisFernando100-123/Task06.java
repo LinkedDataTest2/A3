5515e423e9cc5399157105eda41bfe7b01203a61
Luis Fernando Gil Arroyo <luis.gil.arroyo@alumnos.upm.es>
2018-04-15 22:34:29 +0200

    Add files via upload

---
+++ LuisFernando100-123/Task06.java
@@ -0,0 +1,24 @@
+import java.io.InputStream;
+
+import org.apache.jena.ontology.Individual;
+import org.apache.jena.ontology.OntClass;
+import org.apache.jena.ontology.OntModel;
+import org.apache.jena.ontology.OntModelSpec;
+import org.apache.jena.ontology.OntProperty;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.util.FileManager;
+import org.apache.jena.vocabulary.VCARD;
+
+public class Task06 {
+
+	public static void main(String[] args) {
+		//stub
+	}
+
+	public static int add(int a, int b){
+		//TODO
+		return a+b;
+	}
+
+}
